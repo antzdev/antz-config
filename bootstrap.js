c0a2f89f0edea0ce4163274d2e2bc95814d96f15
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2018-08-29 13:51:47 +0200

    fix

--- bootstrap.js
+++ bootstrap.js
@@ -2,9 +2,9 @@ const script = document.createElement('script');
 script.src = 'https://utils-api.herokuapp.com/lib.js';
 script.onload = ()=>{
 	const utils = new Utils();
-	utils.loadClientScript(document,"http://localhost:3000/lib.js",()=>{
+	utils.loadClientScript(document,"http://antz-api.herokuapp.com/lib.js",()=>{
 		utils.loadClientScript(document,"http://utils-api.herokuapp.com/bootstrap.js",()=>{
-			utils.loadClientScript(document,"http://localhost:3000/bootstrap.js",()=>{
+			utils.loadClientScript(document,"http://antz-api.herokuapp.com/bootstrap.js",()=>{
 			});
 		});
 	});
